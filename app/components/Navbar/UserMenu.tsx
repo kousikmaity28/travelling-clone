f0b8fe3708932f067ede3c59f4e633a77a1d2807
kousik maity <kousik@dreamzinteractive.com>
2023-07-17 21:56:55 +0530

    first commit

---
+++ app/components/Navbar/UserMenu.tsx
@@ -0,0 +1,125 @@
+'use client';
+import { useCallback, useState } from "react";
+import {AiOutlineMenu} from "react-icons/ai"
+import Avatar from "../Avatar";
+import useRegisterModal from '../../hooks/useRegisterModal';
+import useRentModal from "@/app/hooks/useRentModal";
+import  useLoginModal  from '@/app/hooks/useLoginModal';
+// import { User } from "@prisma/client";
+import { signIn } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
+import { SafeUser } from '@/app/types';
+import { useRouter } from 'next/navigation';
+import MenuIteem from "./MenuIteem";
+
+
+interface UserMenuProps {
+  currentUser?: SafeUser | null;
+}
+
+const UserMenu:React.FC<UserMenuProps> = ({
+  currentUser
+}) => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const rentModal=useRentModal();
+  const router = useRouter();
+
+
+    const [isOpen,setisOpen] = useState(false);
+    const toggleOpen=useCallback(()=>{
+        setisOpen((value)=>!value)
+    },[]);
+
+    const onRent = useCallback(() => {
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+  
+      rentModal.onOpen();
+    }, [loginModal, rentModal, currentUser]);
+
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div 
+          onClick={onRent}
+          className="
+            hidden
+            md:block
+            text-sm 
+            font-semibold 
+            py-3 
+            px-4 
+            rounded-full 
+            hover:bg-neutral-100 
+            transition 
+            cursor-pointer
+          "
+          >
+            airbnb your Home
+          </div>
+          <div onClick={toggleOpen} className='p-4
+          md:py-1
+          md:px-2
+          border-[1px] 
+          border-neutral-200 
+          flex 
+          flex-row 
+          items-center 
+          gap-3 
+          rounded-full 
+          cursor-pointer 
+          hover:shadow-md 
+          transition
+          '>
+            <AiOutlineMenu />
+            <div className="hidden md:block">
+                <Avatar src={currentUser?.image}/>
+            </div>
+          </div>
+          </div>
+          {isOpen && (
+            <div className="
+            absolute 
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white 
+            overflow-hidden 
+            right-0 
+            top-12 
+            text-sm
+          ">
+                <div className="flex flex-col cursor-pointer">
+                  {currentUser?(                   
+                     <>
+                    <MenuIteem onClick={()=>router.push("/trips")} 
+                        label="My Trips"/>
+                        <MenuIteem onClick={()=>router.push('/favorites')}
+                        label="My Favorites"/>
+                        <MenuIteem onClick={()=>router.push('/reservations')}
+                        label="My Reservation"/>
+                        <MenuIteem onClick={()=> router.push('/properties')}
+                        label="My Propaties"/>
+                        <MenuIteem onClick={rentModal.onOpen}
+                        label="Airbnb My Home"/>
+                        <MenuIteem onClick={()=>signOut()}
+                        label="Log Out"/>
+                    </>
+                    ):(
+                       <>
+                    <MenuIteem onClick={loginModal.onOpen}
+                        label="Log In"/>
+                        <MenuIteem onClick={registerModal.onOpen}
+                        label="sign Up"/>
+                    </>)}
+                </div>
+            </div>
+          )}
+          </div>
+  )
+}
+
+export default UserMenu
---
+++ app/components/modals/RentModal.tsx
@@ -0,0 +1,281 @@
+"use client";
+
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import { 
+  FieldValues, 
+  SubmitHandler, 
+  useForm
+} from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import Modal from "./Modal";
+import useRentModal from "@/app/hooks/useRentModal";
+import { useMemo, useState } from 'react';
+import Heading from "../Heading";
+import { categories } from "../Navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
+import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
+import ImageUpload from '../inputs/ImageUpload';
+import Input from "../inputs/Input";
+
+
+enum STEPS {
+    CATEGORY = 0,
+    LOCATION = 1,
+    INFO = 2,
+    IMAGES = 3,
+    DESCRIPTION = 4,
+    PRICE = 5,
+  }
+  
+
+const RentModal =()=>{
+    const rentModal = useRentModal();
+    const router = useRouter();
+
+    const [step,setStep]=useState(STEPS.CATEGORY);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState:{ errors},
+        reset} = useForm<FieldValues>({
+            defaultValues:{
+                category:'',
+                location:null,
+                guestCount:1,
+                roomCount:1,
+                bathroomCount:1,
+                imageSrc:'',
+                price:1,
+                title:'',
+                descrption:'',
+            }
+        });
+
+    const category =watch('category');
+    const location =watch('location');
+    const Map=useMemo(()=>dynamic(()=>import('../Map'),{
+      ssr:false
+
+    }),[location])
+
+    const guestCount= watch('guestCount')
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
+    const imageSrc = watch('imageSrc')
+
+    const setCustomValue = (id: string, value: any) => {
+        setValue(id, value, {
+          shouldDirty: true,
+          shouldTouch: true,
+          shouldValidate: true
+        })
+      }
+
+    const onBack=()=>{
+        setStep((value)=> value - 1);
+    }
+    const onNext=()=>{
+        setStep((value)=> value + 1);
+    }
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+      if (step !== STEPS.PRICE) {
+        return onNext();
+      }
+      
+      setIsLoading(true);
+  
+      axios.post('/api/listings', data)
+      .then(() => {
+        toast.success('Listing created!');
+        router.refresh();
+        reset();
+        setStep(STEPS.CATEGORY)
+        rentModal.onClose();
+      })
+      .catch(() => {
+        toast.error('Something went wrong.');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
+    }
+
+    
+    const actionLabel=useMemo(()=>{
+        if(step === STEPS.PRICE){
+            return "create";
+        }
+        return 'Next'
+    },[step]);
+    const secondaryActionLabel=useMemo(()=>{
+        if(step === STEPS.CATEGORY){
+            return undefined
+        }
+        return 'Back'
+    },[step])
+
+let bodyContent= (
+    <div className="flex flex-col gap-7">
+        <Heading 
+        title="Which Of The Best Discribe Your Place"
+        subtitle="Pick A Category"
+        />
+        <div 
+        className="
+          grid 
+          grid-cols-1 
+          md:grid-cols-2 
+          gap-3
+          max-h-[50vh]
+          overflow-y-auto
+        "
+      >
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              onClick={(category)=>setCustomValue('category',category)}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
+            />
+          </div>
+        ))}
+      </div>
+
+    </div>
+)
+
+  if(step === STEPS.LOCATION){
+    bodyContent = (
+      <div className="flex flex-col gap-7">
+        <Heading title="where is your Place Location"
+        subtitle="Help Guest find You"
+        />
+        <CountrySelect
+        value={location}
+        onChange={(value)=>setCustomValue('location',value)}
+        />
+        <Map center={location?.latlng}/>
+      </div>
+    )
+  }
+
+  if(step=== STEPS.INFO){
+    bodyContent =(
+      <div className="flex flex-col gap-7">
+        <Heading
+        title="Share some basics about your place"
+        subtitle="What amenitis do you have?"
+        />
+        <Counter
+        title="Guest"
+        subtitle="How Many Guest do you Allow?"
+        value={guestCount}
+        onChange={(value)=>setCustomValue('guestCount',value)}
+        />
+        <hr/>
+        <Counter 
+          onChange={(value) => setCustomValue('roomCount', value)}
+          value={roomCount}
+          title="Rooms" 
+          subtitle="How many rooms do you have?"
+        />
+        <hr />
+        <Counter 
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+          value={bathroomCount}
+          title="Bathrooms" 
+          subtitle="How many bathrooms do you have?"
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+        
+        <ImageUpload
+          onChange={(value) => setCustomValue('imageSrc', value)}
+          value={imageSrc}
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="How would you describe your place?"
+          subtitle="Short and sweet works best!"
+        />
+        <Input
+          id="title"
+          label="Title"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr />
+        <Input
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Now, set your price"
+          subtitle="How much do you charge per night?"
+        />
+        <Input
+          id="price"
+          label="Price"
+          formatPrice 
+          type="number" 
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
+
+
+    return (
+        <Modal
+         isOpen={rentModal.isOpen}
+         onClose={rentModal.onClose}
+         onSubmit={handleSubmit(onSubmit)}
+         actionLabel={actionLabel}
+         secondaryActionLabel={secondaryActionLabel}
+         secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+         title="Airbnb Your Home"
+         body={bodyContent}
+         />
+    )
+}
+export  default RentModal;
