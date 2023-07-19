496123d319c1e9c3fc0677a87ab7aee10e317f4a
kousik maity <kousik@dreamzinteractive.com>
2023-07-19 12:53:33 +0530

    dynamic

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
-/** @type {import('next').NextConfig} */
+  /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-    images: {
-      domains: [
-        'res.cloudinary.com', 
-        'avatars.githubusercontent.com',
-        'lh3.googleusercontent.com'
-      ]
-    }
+  images: {
+    domains: [
+      'res.cloudinary.com', 
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com'
+    ]
   }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
